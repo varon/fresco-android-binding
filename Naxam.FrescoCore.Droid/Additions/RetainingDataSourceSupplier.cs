2b104aab32c2389ec30c277e34f18613c552e077
varon <varon@users.noreply.github.com>
2018-12-05 15:59:16 +0200

    Cleaner fix for broken RetainingDataSourceSupplier iface impl

--- Naxam.FrescoCore.Droid/Additions/RetainingDataSourceSupplier.cs
+++ Naxam.FrescoCore.Droid/Additions/RetainingDataSourceSupplier.cs
@@ -6,14 +6,10 @@ using System.Threading.Tasks;
 using Android.Runtime;
 
 namespace Com.Facebook.Datasource {
-	public partial class RetainingDataSourceSupplier : global::Java.Lang.Object, global::Com.Facebook.Common.Internal.ISupplier {
-		// Metadata.xml XPath method reference: path="/api/package[@name='com.facebook.datasource']/class[@name='RetainingDataSourceSupplier']/method[@name='get' and count(parameter)=0]"
-		[Register("get", "()Lcom/facebook/datasource/DataSource;", "GetGetHandler")]
-		public virtual unsafe Java.Lang.Object Get() {
-			const string __id = "get.()Lcom/facebook/datasource/DataSource;";
-			var __rm = _members.InstanceMethods.InvokeVirtualObjectMethod(__id, this, null);
-			var uncasted = global::Java.Lang.Object.GetObject<global::Com.Facebook.Datasource.IDataSource>(__rm.Handle, JniHandleOwnership.TransferLocalRef);
-			return (Java.Lang.Object) uncasted;
+	public partial class RetainingDataSourceSupplier {
+
+		Java.Lang.Object Com.Facebook.Common.Internal.ISupplier.Get() {
+			return Android.Runtime.Extensions.JavaCast<Java.Lang.Object>(Get());
 		}
 	}
 }
